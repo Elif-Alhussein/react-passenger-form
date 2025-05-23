c9641b0e2878c38e2d9290be4e7ed99658db8b57
Elif Alhussein <180541610@firat.edu.tr>
2025-05-23 12:22:04 +0300

    update regex

--- src/Schemas/PassengersFormSchemas.js
+++ src/Schemas/PassengersFormSchemas.js
@@ -1,5 +1,7 @@
 import * as Yup from "yup";
 
+const regex = /^[A-Za-z ]*$/;
+
 export const PassengersFormSchemas = Yup.object().shape({
   info: Yup.array().of(
     Yup.object().shape({
@@ -7,10 +9,16 @@ export const PassengersFormSchemas = Yup.object().shape({
       name: Yup.string()
         .min(2, "Too Short!")
         .max(50, "Too Long!")
+        .matches(regex, {
+          message: "Name must be 2-50 characters long and contain only letters",
+        })
         .required("Name is required"),
       surname: Yup.string()
         .min(2, "Too Short!")
         .max(50, "Too Long!")
+        .matches(regex, {
+          message: "Name must be 2-50 characters long and contain only letters",
+        })
         .required("Surname is required"),
       nationality: Yup.string().required("Please select nationality"),
       birthDate: Yup.string().required(
